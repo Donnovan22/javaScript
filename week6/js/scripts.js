31164cd7e1b659198368679afdb820e6cc8051fc
Donnovan22 <val22002@byui.edu>
2022-05-28 22:56:07 -0500

    week06

--- week6/js/scripts.js
+++ week6/js/scripts.js
@@ -1,15 +1,13 @@
 
-
-
 function calculate_bmr(age, gender, height, weight){
   if (gender == "male"){
     //basal metabolic rate (bmr) = (10 x weight in kg) + (6.25 x height in cm) - (5 x age in year) + 5 
     return (10 * weight) + (6.25 * height) - (5 * age) + 5;;
- }
+ };
  if (gender == "female") {
    //basal metabolic rate (bmr) = (10 x weight in kg) + (6.25 x height in cm) - (5 x age in year) - 161
    return (10 * weight) + (6.25 * height) - (5 * age) - 161;
- }
+ };
 }
 
 
@@ -43,19 +41,19 @@ function get_activity_level() {
 
   if(value == "sedentary"){
     return act_lvl.sedentary;
-  }
+  };
   if(value == "lightly-active"){
     return act_lvl.lightlyActive;
-  }
+  };
   if(value == "moderately-active"){
     return act_lvl.moderatelyActive;
-  }
+  };
   if(value == "very-active"){
     return act_lvl.veryActive;
-  }
+  };
   if(value == "extremely-active"){
     return act_lvl.extremelyActive;
-  }
+  };
 }
 
 function measure_tdde(bmr, activity_level) {
