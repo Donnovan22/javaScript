da1e39649bb539fced830ea4c9d4170cefb4bca7
Donnovan22 <val22002@byui.edu>
2022-05-28 22:01:27 -0500

    week06

--- week6/js/scripts.js
+++ week6/js/scripts.js
@@ -1,5 +1,6 @@
 
 
+
 function calculate_bmr(age, gender, height, weight){
   if (gender == "male"){
     //basal metabolic rate (bmr) = (10 x weight in kg) + (6.25 x height in cm) - (5 x age in year) + 5 
@@ -21,7 +22,60 @@ function  determine_bmr() {
   let bmr = calculate_bmr(age, gender, height, weight);
 
   document.querySelector("#bmr").value = bmr;
+  return bmr;
 }
+
+
+
+function get_activity_level() {
+  let select = document.getElementById('activity-level');
+  let value = select.value;
+  if(value == "sedentary"){
+    return 1.2;
+  }
+  if(value == "lightly-active"){
+    return 1.375;
+  }
+  if(value == "moderately-active"){
+    return 1.55;
+  }
+  if(value == "very-activ"){
+    return 1.725;
+  }
+  if(value == "extremely-active"){
+    return 1.9;
+  }
+}
+
+function measure_tdde(bmr, activity_level) {
+  return bmr * activity_level;
+}
+
+
+function tdee() {
+  let tdee = measure_tdde(determine_bmr(), get_activity_level());
+  
+  return tdee;
+}
+
+function determine_goal_weight() {
+  let select = document.getElementById('goal');
+  let value = select.value;
+  let goal;
+  if(value == "loose"){
+    goal = tdee() - 500;
+  }
+  if(value == "maintain"){
+    goal = tdee();
+  }
+  if(value == "gain"){
+    goal = tdee() + 500;
+  }
+  
+  document.querySelector("#tdee").value = goal;
+  return goal;
+}
+
 
 
 
