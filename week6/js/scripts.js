ce50de61b30b8fed6a40cd4e1a61d89fa707f561
Donnovan22 <val22002@byui.edu>
2022-05-28 22:37:42 -0500

    week06

--- week6/js/scripts.js
+++ week6/js/scripts.js
@@ -4,11 +4,11 @@
 function calculate_bmr(age, gender, height, weight){
   if (gender == "male"){
     //basal metabolic rate (bmr) = (10 x weight in kg) + (6.25 x height in cm) - (5 x age in year) + 5 
-    return (10 * weight) + (6.25 * height) - (5 * age) + 5;
+    return (10 * weight) + (6.25 * height) - (5 * age) + 5;;
  }
  if (gender == "female") {
    //basal metabolic rate (bmr) = (10 x weight in kg) + (6.25 x height in cm) - (5 x age in year) - 161
-    return (10 * weight) + (6.25 * height) - (5 * age) - 161;
+   return (10 * weight) + (6.25 * height) - (5 * age) - 161;
  }
 }
 
@@ -20,9 +20,10 @@ function  determine_bmr() {
   let gender =document.querySelector("#gender").value;
 
   let bmr = calculate_bmr(age, gender, height, weight);
+  let rounded_bmr = Math.round(bmr);
 
-  document.querySelector("#bmr").value = bmr;
-  return bmr;
+  document.querySelector("#bmr").value = rounded_bmr;
+  return rounded_bmr;
 }
 
 
@@ -30,32 +31,42 @@ function  determine_bmr() {
 function get_activity_level() {
   let select = document.getElementById('activity-level');
   let value = select.value;
+
+  const act_lvl = {
+    sedentary: 1.2, 
+    lightlyActive: 1.375, 
+    moderatelyActive: 1.55, 
+    veryActive:1.725,
+    extremelyActive: 1.9
+  };
+
+
   if(value == "sedentary"){
-    return 1.2;
+    return act_lvl.sedentary;
   }
   if(value == "lightly-active"){
-    return 1.375;
+    return act_lvl.lightlyActive;
   }
   if(value == "moderately-active"){
-    return 1.55;
+    return act_lvl.moderatelyActive;
   }
-  if(value == "very-activ"){
-    return 1.725;
+  if(value == "very-active"){
+    return act_lvl.veryActive;
   }
   if(value == "extremely-active"){
-    return 1.9;
+    return act_lvl.extremelyActive;
   }
 }
 
 function measure_tdde(bmr, activity_level) {
-  return bmr * activity_level;
+  return  bmr * activity_level;
 }
 
 
 function tdee() {
   let tdee = measure_tdde(determine_bmr(), get_activity_level());
-  
-  return tdee;
+  let roundTDEE = Math.round(tdee);
+  return roundTDEE;
 }
 
 function determine_goal_weight() {
